4fbe57f724124ea68e4b7443b0136100f910168e
sasha (42) <facosta@student.42madrid.com>
2025-01-05 15:52:10 +0100

    🎨 remove in-function comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: facosta <facosta@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 22:16:18 by facosta           #+#    #+#             */
-/*   Updated: 2025/01/05 11:33:28 by facosta          ###   ########.fr       */
+/*   Updated: 2025/01/05 15:51:21 by facosta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,15 @@ size_t	gnl_strlen(char *s)
 	return (len);
 }
 
+// Given a line(by reference), trim everything after the first \n and overwrite
+// the contents of buffer with the remainder
+// E.g.,
+// *p_line = "En un lugar de la mancha, de cuyo nombre\nno quiero\nacordarme\n"
+// -->     = "En un lugar de la mancha, de cuyo nombre\n"
+// buffer  = "nombre\nno quiero\nacordarme"
+// -->     = "no quiero\nacordarme"
+// OBS: as we only mean to pass a static variable as buffer, the original
+// buffer still gets overwritten even though you are not passing it by ref.
 static void	move_remainder_to_buffer(string *p_line, string buffer)
 {
 	ssize_t	remainder_len;
@@ -32,23 +41,30 @@ static void	move_remainder_to_buffer(string *p_line, string buffer)
 	line_len = 0;
 	while (line[line_len] != '\n' && line[line_len])
 		line_len++;
-	if (line[line_len] == '\n') // si he parado por encontrarme un salto de linea, añade uno mas a la len
+	if (line[line_len] == '\n')
 		line_len++;
 	remainder_len = (gnl_strlen(line) - line_len);
-	if (remainder_len != 0) //i.e. if there is some remainder (i.e. len not 0)
-		gnl_strlcpy(buffer, line + line_len, remainder_len + 1); // copy the contents of line, starting right after the \n, onto buffer
+	if (remainder_len != 0)
+		gnl_strlcpy(buffer, line + line_len, remainder_len + 1);
 	else
 		buffer[0] = '\0';
-	// OBS: note how there is no chance of the remainder not fitting entirely
-	// in the buffer var, as the remainder has at most buffer-len length
 	line = gnl_substr(line, 0, line_len + 1);
 	free(*p_line);
 	*p_line = line;
 }
 
+// Read chunks of size BUFFER_SIZE until you find \n, EOF or there is an error
+// during reading. Every chunk you read is appended to composed_line and when
+// you've stopped reading, put that composed_line into line (passed by ref).
+// E.g., at the end, results will be sth like this:
+// *p_line = "En un lugar de la mancha, de cuyo nombre\nno quiero\nacordarme\n"
+// buffer  = "nombre\nno quiero\nacordarme"
+// NOTE: The buffer will be a "valid" string, i.e. nul-terminated
+// NOTE: We use type `ssize_t` for read_bytes to allow for negative numbers
+// which would mean the read failed
 static void	read_until_newline_in_buffer(int fd, string *p_line, string buffer)
 {
-	ssize_t	read_bytes; // we use ssize_t to allow for negative numbers, as read can be <0
+	ssize_t	read_bytes;
 	string	composed_line;
 
 	composed_line = malloc(1 * sizeof(char));
@@ -60,17 +76,12 @@ static void	read_until_newline_in_buffer(int fd, string *p_line, string buffer)
 	}
 	composed_line[0] = '\0';
 	gnl_strjoin(&composed_line, *p_line);
-	read_bytes = 1;  // whatever positive value to start loop
-	// while there is no newline in buffer and we've been able to read as many
-	// bytes as we want
+	read_bytes = 1;
 	while (!(gnl_strchr(composed_line, '\n')) && read_bytes > 0)
 	{
-		// continue to read, and document how many bytes you've been able 2 read
-		read_bytes = read(fd, buffer, BUFFER_SIZE);  // try to read
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
 		if ((read_bytes < 0) || (read_bytes == 0 && !composed_line))
 		{
-			// Cuando falla la lectura o encuentro final de archivo, tienes que
-			// devolver NULL en gnl (x el enunciado)
 			free(composed_line);
 			free(*p_line);
 			*p_line = NULL;
@@ -83,15 +94,18 @@ static void	read_until_newline_in_buffer(int fd, string *p_line, string buffer)
 			*p_line = NULL;
 			return ;
 		}
-		buffer[read_bytes] = '\0'; // convert the buffer to a "valid string"
+		buffer[read_bytes] = '\0';
 		gnl_strjoin(&composed_line, buffer);
 	}
 	free(*p_line);
 	*p_line = composed_line;
 }
 
-// Main function is just in charge of doing some clearing checks and allocating
-// 1byte into the resulting line variable->just to initialize it to something
+// * Some initial checks
+// * allocating 1byte into the resulting line variable, just to initialize it
+//   to something
+// * if the buffer contains stuff, i.e. a remainder from reading a previous
+//   line => put its contents in line
 char	*get_next_line(int fd)
 {
 	char	*line;
@@ -104,7 +118,7 @@ char	*get_next_line(int fd)
 	if (!line)
 		return (NULL);
 	line[0] = '\0';
-	if (buffer[0] != '\0') // i.e. buffer contains some "remainder" from reading the line before
+	if (buffer[0] != '\0')
 		gnl_strjoin(&line, buffer);
 	read_until_newline_in_buffer(fd, &line, buffer);
 	if (!line)
